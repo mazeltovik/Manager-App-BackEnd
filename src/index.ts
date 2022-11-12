6252bbb1b5aa6a9958d0d20a697f489db268d0c9
mazeltovik <mazeltovik@gmail.com>
2022-11-12 23:03:26 +0300

    feat:start server

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import { PORT } from './constants';
+
+import * as serverService from './services/server.service';
+
+
+(async () => {
+  try {
+    await mongoose.connect('mongodb+srv://mazeltovik:dezik1972@cluster0.oacyi9a.mongodb.net/?retryWrites=true&w=majority');
+    serverService.server.listen(process.env.PORT || PORT, function () {
+      console.log('Сервер ожидает подключения...');
+    })
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+
+
+process.on('SIGINT', async () => {
+  await mongoose.disconnect();
+  process.exit();
+});
