68caf2436e870a1df463c27f8ed8c21f674dead7
mazeltovik <mazeltovik@gmail.com>
2022-11-13 09:56:16 +0300

    fix:update password to mongo db

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://mazeltovik:dezik1972@cluster0.oacyi9a.mongodb.net/?retryWrites=true&w=majority');
+    await mongoose.connect('mongodb+srv://mazeltovik:HO04ImVAWxVUCocV@cluster0.oacyi9a.mongodb.net/?retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
